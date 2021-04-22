bb4b79e62e16eea2aea9626f6aeed9382a46d114
sanya22chhabra <sanya22chhabra@gmail.com>
2021-04-23 00:56:07 +0530

    seventh Commit in Windows Environment

---
+++ ifelifelse2.py
@@ -0,0 +1,10 @@
+
+inp=input("Enter abbreviation: ")
+if(inp=="lol"):
+    print("Laughing out loud")
+elif(inp=="rofl"):
+    print("Rolling on the floor laughing")
+elif(inp=="lmk"):
+    print("Let me know")
+elif(inp=="smh"):
+    print("Shaking my head")
